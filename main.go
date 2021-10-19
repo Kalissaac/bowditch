a8799eb6644eda4129aab3e4c353c36db3907582
Kalissaac <kalissaac@protonmail.com>
2021-10-19 13:46:05 -0700

    Fix code

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+const (
+	BREAKFAST = "Breakfast"
+	LUNCH     = "Lunch"
+	DINNER    = "Dinner"
+)
+
 func main() {
 	app := fiber.New()
 
@@ -30,13 +36,19 @@ func main() {
 	})
 
 	app.Get("/crossroads", func(c *fiber.Ctx) error {
-		return c.JSON(getData("crossroads", ""))
+		return c.JSON(map[string][]MealData{
+			"data": {
+				getData("Crossroads", BREAKFAST),
+				getData("crossroads", LUNCH),
+				getData("crossroads", DINNER),
+			},
+		})
 	})
 	app.Get("/crossroads/lunch", func(c *fiber.Ctx) error {
-		return c.JSON(getData("crossroads", "lunch"))
+		return c.JSON(getData("crossroads", LUNCH))
 	})
 	app.Get("/crossroads/dinner", func(c *fiber.Ctx) error {
-		return c.JSON(getData("crossroads", "dinner"))
+		return c.JSON(getData("crossroads", DINNER))
 	})
 
 	if runtime.GOOS == "darwin" {
@@ -46,12 +58,41 @@ func main() {
 	}
 }
 
-func getData(restaurant string, meal string) map[string]interface{} {
+type MealData struct {
+	Name     string        `json:"name"`
+	Sections []MealSection `json:"sections"`
+}
+
+type MealSection struct {
+	Name  string     `json:"name"`
+	Items []MealItem `json:"items"`
+}
+
+type MealItem struct {
+	Name string `json:"name"`
+}
+
+func getData(restaurant string, meal string) MealData {
+	d := MealData{
+		Name: meal,
+	}
 	c := colly.NewCollector()
 
 	// Find and visit all links
-	c.OnHTML(".cafe-location .Crossroads .meal-period ."+meal+" ", func(e *colly.HTMLElement) {
-		// e.Request.Visit(e.Attr("href"))
+	c.OnHTML(".cafe-location .Crossroads .meal-period ."+meal+" .cat-name", func(e *colly.HTMLElement) {
+		s := MealSection{}
+		childNodes := e.DOM.Children()
+
+		s.Name = childNodes.First().Text()
+		e.ForEach(".recip", func(_ int, recipeElement *colly.HTMLElement) {
+			item := MealItem{
+				Name: recipeElement.DOM.Children().First().Text(),
+			}
+
+			s.Items = append(s.Items, item)
+		})
+
+		d.Sections = append(d.Sections, s)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -60,7 +101,5 @@ func getData(restaurant string, meal string) map[string]interface{} {
 
 	c.Visit("https://caldining.berkeley.edu/menus/")
 
-	return map[string]interface{}{
-		"stautus": "ok",
-	}
+	return d
 }
